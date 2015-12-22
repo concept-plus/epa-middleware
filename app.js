7500adfe2e23e89c9daf047fdff1f841690c6a0d
Mike Mathis <mmathis@conceptplusllc.com>
2015-12-22 14:23:34 -0500

    Initial commit with services for querying UV and AQI

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+var PORT = 8181;
+
+app.set('x-powered-by', false);
+app.set('Content-Type', 'application/json');
+
+
+app.use('/relay/uv', function(req, res, next) {
+
+    var zipcode = req.query.zipcode;
+    console.log('querying UV with zipcode ' + zipcode);
+
+    var url = 'http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/' + zipcode + '/JSON';
+    var req = http.get(url, function(response) {
+        // console.log(JSON.stringify(Object.keys(response)));
+        // console.log('status=' + response.statusCode + ', url=' + response.url);
+
+        response.on('data', function(body) {
+            console.log('body = ' + body);
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Content-Type', 'application/json');
+            res.status(response.statusCode).send(body);
+        });
+    });
+    req.end();
+});
+
+app.use('/relay/aqi', function(req, res, next) {
+
+    var zipcode = req.query.zipcode;
+    console.log('querying AQI with zipcode ' + zipcode);
+
+    var url = 'http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=' + zipcode + '&distance=25&API_KEY=193C637C-1EDD-4F7A-A9FC-618AFF9FFD2D';
+    var req = http.get(url, function(response) {
+        // console.log(JSON.stringify(Object.keys(response)));
+        response.on('data', function(body) {
+            console.log('body = ' + body);
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Content-Type', 'application/json');
+            res.status(response.statusCode).send(body);
+        });
+    });
+    req.end();
+});
+
+var server = app.listen(PORT, function() {
+    console.log('Listening on port ' + PORT);
+});
